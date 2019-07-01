fa8c2f51bdcd6c791a8554dec5400ece9fd04a13
Yash Thakkar <nerdyash@yahoo.com>
2019-07-01 10:33:26 -0400

    Updates

---
+++ precache-manifest.7fe5c1ebfe53c50b54067b34603ca60c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186b66ef2263c127dc486b69e0dafbde",
+    "url": "/React-todo/index.html"
+  },
+  {
+    "revision": "3c9296e268c0f5b850a6",
+    "url": "/React-todo/static/css/main.3507c367.chunk.css"
+  },
+  {
+    "revision": "98ac9e6c57bf244c3dc7",
+    "url": "/React-todo/static/js/2.12e6d4b0.chunk.js"
+  },
+  {
+    "revision": "3c9296e268c0f5b850a6",
+    "url": "/React-todo/static/js/main.89b9fe67.chunk.js"
+  },
+  {
+    "revision": "45aea7dcebb6519058d1",
+    "url": "/React-todo/static/js/runtime~main.d5f1644e.js"
+  }
+]);
